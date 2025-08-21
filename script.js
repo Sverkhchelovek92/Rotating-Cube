9439946ada42435bdbc65ad377d12f7a9c0930a8
Miroslav Gurnyak <mirmiroslav@gmail.com>
2025-08-21 13:05:47 +0300

    stuff

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ const ctx = canvas.getContext('2d')
 
 ctx.fillStyle = '#1f1f21'
 ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+// Some constants
+
+const cubeColor = '#d11f31'
+const velocityX = 0.12
+const velocityY = 0.13
+const velocityZ = 0.9
+
+var w = canvas.width
+var h = canvas.height
+
+//
+
+ctx.strokeStyles = cubeColor
+ctx.lineWidth = w / 10
+ctx.lineCap = 'round'
