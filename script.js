5289b4c0a227f556da7b44b5501397ce078313dc
Miroslav Gurnyak <mirmiroslav@gmail.com>
2025-08-30 12:09:12 +0300

    cube

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const h = canvas.height
 //
 
 ctx.strokeStyle = cubeColor
-ctx.lineWidth = w / 10
+ctx.lineWidth = w / 100
 ctx.lineCap = 'round'
 
 // cube
@@ -25,17 +25,63 @@ ctx.lineCap = 'round'
 const cx = w / 2
 const cy = h / 2
 const cz = 0
-const size = h / 2
+const size = h / 4
+
+// vertices and edges
+
+class POINT3D {
+  constructor(x, y, z) {
+    this.x = x
+    this.y = y
+    this.z = z
+  }
+}
+
+let vertices = [
+  new POINT3D(cx - size, cy - size, cz - size),
+  new POINT3D(cx + size, cy - size, cz - size),
+  new POINT3D(cx + size, cy + size, cz - size),
+  new POINT3D(cx - size, cy + size, cz - size),
+  new POINT3D(cx - size, cy - size, cz + size),
+  new POINT3D(cx + size, cy - size, cz + size),
+  new POINT3D(cx + size, cy + size, cz + size),
+  new POINT3D(cx - size, cy + size, cz + size),
+]
+
+let edges = [
+  [0, 1],
+  [1, 2],
+  [2, 3],
+  [3, 0],
+  [4, 5],
+  [5, 6],
+  [6, 7],
+  [7, 4],
+  [0, 4],
+  [1, 5],
+  [2, 6],
+  [3, 7],
+]
 
 // animation
 
 var timeDelta,
   timeLast = 0
+
 requestAnimationFrame(loop)
 
 function loop(timeNow) {
   timeDelta = timeNow - timeLast
   timeLast = timeNow
+
+  ctx.fillRect(0, 0, w, h)
+
+  for (let edge of edges) {
+    ctx.beginPath()
+    ctx.moveTo(vertices[edge[0]].x, vertices[edge[0]].y)
+    ctx.lineTo(vertices[edge[1]].x, vertices[edge[1]].y)
+    ctx.stroke()
+  }
 
   requestAnimationFrame(loop)
 }
